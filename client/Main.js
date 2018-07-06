649945b5602feecee1787d188f542bba263b0044
Liam Neville <lineville@vassar.edu>
2018-07-06 11:32:41 -0400

    api for teams working

--- client/Main.js
+++ client/Main.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react'
 import { HashRouter as Router } from 'react-router-dom'
+import axios from 'axios'
+
 export default class Main extends Component {
   constructor() {
     super()
     this.state = {
       currentRound: 1,
+      teams: [],
       // will need to store the data from the sliders
     }
   }
 
   //Component did mount should render the round 1 bracket
-
+  async componentDidMount() {
+    const teams = await axios.get(`/api/teams/`)
+    console.log(teams)
+    this.setState({ teams: teams.data })
+  }
   render() {
     // Should render the full bracket and form component
     return (
-      <Router>
-        <h1>WELCOME TO NBA SIMULATOR!</h1>
-      </Router>
+      // <Router>
+      <div>
+        <h1>2018 NBA Playoffs Simulator!</h1>
+        <div>
+          <ul>
+            {this.state.teams.map((team, idx) => (
+              <li key={idx}>{team.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      // </Router>
     )
   }
 }
--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,8 @@ const { db, Player, Team } = require('../db/index')
 
 router.get('/teams/', async (req, res, next) => {
   const allTeams = await Team.findAll()
-  res.json(allTeams.data)
+  console.log(allTeams)
+  res.json(allTeams)
 })
 
 module.exports = router
