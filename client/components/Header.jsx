2972f4f5b63e8b10b8f0c39009f1e93e007302e2
Liam Neville <lineville@vassar.edu>
2018-08-01 17:31:54 -0400

    removed matchup as it is not working with styles
    Signed-off-by: Liam Neville <lineville@vassar.edu>

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,17 +3,11 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <nav className="navbar navbar-light bg-light">
+      <h1 className="title">
         <a className="navbar-brand" href="#">
-          <img
-            src="nba-logo.png"
-            id="nba-logo"
-            className="d-inline-block align-top"
-            alt=""
-          />
-        </a>
-        <h1 className="ui-header">2018 NBA Playoffs Simulator</h1>
-      </nav>
+          <img src="nba-logo.png" id="nba-logo" alt="nba logo" />
+        </a>2018 NBA Playoffs Simulator
+      </h1>
     </div>
   )
 }
