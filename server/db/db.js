c01a459453e3c88e5c0a3bed2f4dff4685572fc1
Liam Neville <lineville@vassar.edu>
2018-08-06 09:50:39 -0400

    updating db init
    Signed-off-by: Liam Neville <lineville@vassar.edu>

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize')
 
-const dbName = 'playoffs' + (process.env.NODE_ENV === 'test' ? '-test' : '')
-
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/playoffs`,
   {
     logging: false,
     operatorsAliases: false,
