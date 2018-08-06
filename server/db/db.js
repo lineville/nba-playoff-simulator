f6bd728280e3b3c19c90252fb6e3ad0a32489f21
Liam Neville <lineville@vassar.edu>
2018-08-06 10:19:12 -0400

    went back to normal seed file
    Signed-off-by: Liam Neville <lineville@vassar.edu>

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,7 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/playoffs`,
-  {
-    logging: false,
-    operatorsAliases: false,
-  }
-)
+const db = new Sequelize(`postgres://localhost:5432/playoffs`, {
+  logging: false,
+  operatorsAliases: false,
+})
 module.exports = db
